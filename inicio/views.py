0aeb11b67b9fc76c7cd67d0685d8eaac1296f876
Alfredo Paz Valderrama <Alfredo Paz>
2021-06-02 21:39:55 -0500

    Se agregaron mensajes para depuración en ambas vistas

--- inicio/views.py
+++ inicio/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def myHomeView(*args, **kwars):
+  print(args, kwars)
   return HttpResponse('<h1>Hola Mundo desde Django</h1>')
 
 def anotherView(request, *args, **kwars):
+  print(args, kwars)
+  print(request.user)
   return HttpResponse('<h1>Sólo otra página</h1>')
