a254e52e4d4d8067d24936614144c46df17122ec
Alfredo Paz Valderrama <Alfredo Paz>
2021-06-02 21:32:52 -0500

    Se creo una nueva vista y se le asoció un patron de URL

--- inicio/views.py
+++ inicio/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def myHomeView(*args, **kwars):
   return HttpResponse('<h1>Hola Mundo desde Django</h1>')
+
+def anotherView(request, *args, *kwars):
+  return HttpResponse('<h1>Sólo otra página</h1>')
--- listaContactos/urls.py
+++ listaContactos/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from inicio.views import myHomeView
+from inicio.views import myHomeView, anotherView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', myHomeView, name='Página de Inicio'),
-    path('another/', myHomeView),
+    path('another/', anotherView),
 ]
