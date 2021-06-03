f2040dff8bd961c99aa4e620ac9c85bd8cf344fc
Alfredo Paz Valderrama <Alfredo Paz>
2021-06-02 21:04:31 -0500

    Se agregó el modelo un nuevo campo boolean, se hizo make migrations y se dio un valor por defecto, se hizo migrate

--- personas/models.py
+++ personas/models.py
@@ -4,3 +4,4 @@ class Persona(models.Model):
   nombres = models.CharField(max_length = 100)
   apellidos = models.CharField(max_length = 100)
   edad = models.IntegerField()
+  donador = models.BooleanField()
