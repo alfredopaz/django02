fa40275f0504353b8d87a5e36e51f0e200c2d5fa
Alfredo Paz Valderrama <Alfredo Paz>
2021-06-02 21:35:51 -0500

    Se corrigió los argumentos de la nueva vista

--- inicio/views.py
+++ inicio/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 def myHomeView(*args, **kwars):
   return HttpResponse('<h1>Hola Mundo desde Django</h1>')
 
-def anotherView(request, *args, *kwars):
+def anotherView(request, *args, **kwars):
   return HttpResponse('<h1>Sólo otra página</h1>')
