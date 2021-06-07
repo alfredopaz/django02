ef0be17cc2617a23936e9643a4d5177a39b2076e
Alfredo Paz Valderrama <Alfredo Paz>
2021-06-06 21:32:51 -0500

    Se creo un formulario plano para una búsqueda en un APP externo

--- listaContactos/urls.py
+++ listaContactos/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from inicio.views import myHomeView, anotherView
-from personas.views import personaTestView, personaCreateView
+from personas.views import personaTestView, personaCreateView, searchForHelp
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +24,5 @@ urlpatterns = [
     path('another/', anotherView),
     path('persona/', personaTestView, name = 'Test Persona'),
     path('add/', personaCreateView, name = 'createPersona'),
+    path('search/', searchForHelp, name = 'buscar'),
 ]
